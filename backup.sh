a2b33f1b2849dd37677c4c12cbc68679f47b0e4f
Xan Manning <xan.manning@gmail.com>
2013-03-27 13:43:26 +0000

    Annotated backup

--- backup.sh
+++ backup.sh
@@ -39,32 +39,64 @@ case `uname` in
 		;;
 esac
 
+# Count variables
+tb=${#backup_dots[@]}
+cb=0
+eb=0
+
 # Backup every file that is in the array
+echo "Backing up $hostdir ..."
 for b in ${backup_dots[@]} ; do
 	if [ -e ~/.$b ] ; then
 		if [ -d ~/.$b ] ; then
 			mkdir -p $(dirname ./$hostdir/$b)
 			$($dircpcmd ~/.$b ./$hostdir/$b)
+			let "cb++"
 		elif [ -f ~/.$b ] ; then
 			mkdir -p $(dirname ./$hostdir/$b)
 			cat ~/.$b > ./$hostdir/$b
+			let "cb++"
 		else
 			echo "Error backing up .$b"
+			let "eb++"
 		fi
 	fi
 done
 
+echo "$cb items backed up. Total possible: $tb."
+if [ $eb -gt 1 ] ; then
+	echo "$eb items failed to backup."
+fi
+
+echo ""
+
+# Count variables
+ce=0
+fe=0
+
 # Delete the files in the backup folder we don't want
+echo "Excluding files from backup ..."
 for e in ${backup_exclude[@]} ; do
 	if [ $(ls ./$hostdir/$e 2> /dev/null | wc -l) -gt 1 ] ; then
 		rm -r ./$hostdir/$e
+		let "ce++"
 	else	
 		if [ -d ./$hostdir/$e ] ; then
 			rm -r ./$hostdir/$e
+			let "ce++"
 		elif [ -f ./$hostdir/$e ] ; then
 			rm ./$hostdir/$e
+			let "ce++"
 		else
-			excluded=$e
+			let "fe++"
 		fi
 	fi
 done
+
+echo "$ce items excluded from backup."
+if [ $fe -gt 1 ] ; then
+	echo "$fe items not found in this backup."
+fi
+
+echo ""
+echo "Done!"
