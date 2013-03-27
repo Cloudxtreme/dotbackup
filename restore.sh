13886e2820d39733ce918ce9a01e2199a0a5c0b5
Xan Manning <xan.manning@gmail.com>
2013-03-27 23:28:00 +0000

    fixed recursive backup error

--- restore.sh
+++ restore.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
-#
-#	CONFIG
-#
-
-# Define the dot-files and dot-directories to backup (omit the '.').
-# This is relative to home (~/).
-backup_dots=(zshrc zprofile zlogin zlogout zsh vimrc vim)
-
-# Define the stuff you wish to exclude, either the file, directory 
-# or wildcard files. (removes file from backup.)
-backup_exclude=(zsh/zsh_history vim/backup/* vim/tmp/*)
-
-# Should we back up to a git repository? (This just does a basic 
-# add, commit, push).
-git_backup=true
-
-#
-#	/CONFIG
-#
-
-
-
-# This variable defines the directory to store this host's backup.
-hostdir=$HOSTNAME
-
-# If the backup dir doesn't exist, create it!
-if [ ! -d ./$hostdir ] ; then
-	mkdir ./$HOSTNAME
+# Define where the backup file is, either hostname or argument
+if [ ! $1 ] ; then
+	hostdir=$HOSTNAME
+else
+	hostdir=$1
 fi
 
-# Define function for copying directories.
+# Define function for copying directories
 case `uname` in
 	Darwin)
 		dircpcmd="cp -r -n"
@@ -43,78 +20,26 @@ case `uname` in
 		;;
 esac
 
-# Count variables
-tb=${#backup_dots[@]}
-cb=0
-eb=0
-
-# Backup every file that is in the array
-echo "Backing up $hostdir ..."
-for b in ${backup_dots[@]} ; do
-	if [ -e ~/.$b ] ; then
-		if [ -d ~/.$b ] ; then
-			mkdir -p $(dirname ./$hostdir/$b)
-			if [ -d ./$hostdir/$b ] ; then
-				rm -r ./$hostdir/$b
+# Check backups exist and if so restore them.
+if [ -e ./$hostdir/ ] ; then
+	echo "Restoring $hostdir ... "
+	for i in $(ls ./$hostdir/) ; do
+		if [ -e ./$hostdir/$i ] ; then
+			if [ -d ./$hostdir/$i ] ; then
+				if [ -d ~/.$i ] ; then
+					rm -r ~/.$i
+				fi
+				$($dircpcmd ./$hostdir/$i ~/.$i)
+			elif [ -f ./$hostdir/$i ] ; then
+				cat ./$hostdir/$i > ~/.$i
+			else
+				echo "Error restoring $i"
 			fi
-			$($dircpcmd ~/.$b ./$hostdir/$b)
-			let "cb++"
-		elif [ -f ~/.$b ] ; then
-			mkdir -p $(dirname ./$hostdir/$b)
-			cat ~/.$b > ./$hostdir/$b
-			let "cb++"
-		else
-			echo "Error backing up .$b"
-			let "eb++"
 		fi
-	fi
-done
-
-echo "$cb items backed up. Total possible: $tb."
-if [ $eb -gt 1 ] ; then
-	echo "$eb items failed to backup."
-fi
-
-echo ""
-
-# Count variables
-ce=0
-fe=0
-
-# Delete the files in the backup folder we don't want
-echo "Excluding files from backup ..."
-for e in ${backup_exclude[@]} ; do
-	if [ $(ls ./$hostdir/$e 2> /dev/null | wc -l) -gt 1 ] ; then
-		rm -r ./$hostdir/$e
-		let "ce++"
-	else	
-		if [ -d ./$hostdir/$e ] ; then
-			rm -r ./$hostdir/$e
-			let "ce++"
-		elif [ -f ./$hostdir/$e ] ; then
-			rm ./$hostdir/$e
-			let "ce++"
-		else
-			let "fe++"
-		fi
-	fi
-done
-
-echo "$ce items excluded from backup."
-if [ $fe -gt 1 ] ; then
-	echo "$fe items not found in this backup."
+	done
+else
+	echo "No backup available for $hostdir!"
 fi
 
 echo ""
 echo "Done!"
-echo ""
-
-# Backing up to git if requested in config.
-if [ -d "./.git" ] && $git_backup ; then
-	dtn=$(date +"%Y-%m-%d")
-	echo "Backing up to remote git repository ..."
-	git add .
-	git commit -m "Backup of $hostdir; ($dtn)"
-	git push --all
-	echo "Done!"
-fi
